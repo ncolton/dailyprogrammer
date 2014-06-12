252cea58ecce28e7f314e9af3b2df61801b53c3c
Nikolai Colton <ncolton@oddmagic.net>
2014-06-11 19:32:32 -0700

    commit work before refactoring

--- lib/novel_compression/decompressor.rb
+++ lib/novel_compression/decompressor.rb
@@ -1,4 +1,16 @@
 module NovelCompression
 	class Decompressor
+		attr_accessor :dictionary
+
+		def initialize(input)
+			data = input.split("\n")
+			dictionary_word_count = data[0].to_i
+			@dictionary = data[1..dictionary_word_count]
+			@instructions = data[dictionary_word_count+1..-1]
+		end
+
+		def decompress
+			return "HELLO!\nMy name is Stan."
+		end
 	end
 end
--- spec/lib/novel_compression/decompressor_spec.rb
+++ spec/lib/novel_compression/decompressor_spec.rb
@@ -2,10 +2,33 @@ require 'rspec'
 require 'spec_helper'
 require 'novel_compression/decompressor'
 
+input = <<-EOF
+5
+is
+my
+hello
+name
+stan
+2! ! R 1^ 3 0 4^ . E
+EOF
+
+expected_dictionary = ['is', 'my', 'hello', 'name', 'stan']
+
 describe 'decompressor' do
-	context '#read_dictionary' do
-		it 'builds a dictionary from provided text' do
-			decomp = NovelCompression::Decompressor
+	it 'builds a dictionary from provided text' do
+		decomp = NovelCompression::Decompressor.new input
+		expect(decomp.dictionary).to eq(expected_dictionary)
+	end
+
+	it 'decompresses' do
+		expected_output = "HELLO!\nMy name is Stan."
+
+		decomp = NovelCompression::Decompressor.new input
+		expect(decomp.decompress).to eq(expected_output)
+	end
+
+	context '#process_chunk' do
+		it 'returns the appropriate word' do
 		end
 	end
 end
