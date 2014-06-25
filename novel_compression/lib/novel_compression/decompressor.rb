05005aedf6e24c9b4366179b0ea98c853034e7d6
Nikolai Colton <ncolton@oddmagic.net>
2014-06-24 20:31:05 -0700

    spacing conversion completed

--- novel_compression/lib/novel_compression/decompressor.rb
+++ novel_compression/lib/novel_compression/decompressor.rb
@@ -1,3 +1,5 @@
+require 'novel_compression/spacing'
+
 module NovelCompression
 	class Decompressor
 		attr_accessor :dictionary
@@ -10,59 +12,41 @@ module NovelCompression
 			# Extract the dictionary from the input
 			word_count.times { @dictionary << @input.gets.strip }
 			@chunk_regex = /\A(?<number>\d*)(?<operator>[^\d\s]*)\z/
-			@output = ""
 			@decoded_chunks = []
 			@decoded_chunks_types = []
 		end
 
 		def process_instructions(instructions)
 			accepted_punctuation =['.', ',', '?', '!', ';', ':']
-			@previous_do_not_pad = true
-			@do_not_pad = false
 			chunks = instructions.split
 			chunks.each do |chunk|
 				match = @chunk_regex.match chunk
 				case match[:operator]
 				when 'E' # end of input
-					return @output
+					return build_output
 				when '^' # capitalize the word
-					text = @dictionary[decode_index match[:number]].capitalize
-					@decoded_chunks << text
+					@decoded_chunks << @dictionary[decode_index match[:number]].capitalize
 					@decoded_chunks_types << Spacing::WORD
 				when 'R' # new line
-					text = '\n'
-					@do_not_pad = true
-					@decoded_chunks << text
+					@decoded_chunks << '\n'
 					@decoded_chunks_types << Spacing::NEWLINE
 				when '-' # hyphenate previous and next word
-					text = '-'
-					@do_not_pad = true
-					@decoded_chunks << text
+					@decoded_chunks << '-'
 					@decoded_chunks_types << Spacing::HYPHEN
 				when *accepted_punctuation
 					if match[:operator] == '!' and match[:number].length > 0
 						# if N! make word full caps
-						text = @dictionary[decode_index match[:number]].upcase
-						@decoded_chunks << text
+						@decoded_chunks << @dictionary[decode_index match[:number]].upcase
 						@decoded_chunks_types << Spacing::WORD
 					else
 						# if punctuation by itself
-						text = match[:operator]
-						text << ' '
-						@do_not_pad = true
 						@decoded_chunks << match[:operator]
 						@decoded_chunks_types << Spacing::PUNCTUATION
 					end
 				when '' # just a word
-					text = @dictionary[decode_index match[:number]]
-					@decoded_chunks << text
+					@decoded_chunks << @dictionary[decode_index match[:number]]
 					@decoded_chunks_types << Spacing::WORD
 				end
-
-				if should_space_be_added
-					@output << " "
-				end
-				@output << text
 			end
 		end
 
@@ -70,19 +54,18 @@ module NovelCompression
 			index = Integer(s, 10)
 		end
 
-		def should_space_be_added
-			retval = true
-			if @previous_do_not_pad
-				retval = false
-			elsif @do_not_pad
-				retval = false
-			else
-				retval = true
+		def build_output
+			output_string = "#{@decoded_chunks[0]}"
+			index = 1
+			while index + 1 <= @decoded_chunks_types.length
+				if Spacing::should_space? @decoded_chunks_types[(index - 1)..index]
+					output_string << " "
+				end
+				output_string << @decoded_chunks[index]
+				index = index + 1
 			end
 
-			@previous_do_not_pad = @do_not_pad
-			@do_not_pad = false
-			return retval
+			return output_string
 		end
 	end
 end
