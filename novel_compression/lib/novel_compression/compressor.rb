6ac6c73dc75fcac78ace149b6a5cf2a7f1f4823c
Nikolai Colton <ncolton@oddmagic.net>
2014-06-27 17:12:03 -0700

    tokenizing working, I think...

--- novel_compression/lib/novel_compression/compressor.rb
+++ novel_compression/lib/novel_compression/compressor.rb
@@ -1,4 +1,16 @@
 module NovelCompression
 	class Compressor
+		def initialize(input)
+			@input = input
+		end
+
+		def tokenize
+			regex = /\b?(\w+|[.,?!;:\n])\b?/
+			tokens = []
+			@input.readlines.each do |line|
+				tokens << line.scan(regex)
+			end
+			return tokens.flatten
+		end
 	end
 end
--- novel_compression/spec/lib/novel_compression/compressor_spec.rb
+++ novel_compression/spec/lib/novel_compression/compressor_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 require 'novel_compression/compressor'
 
 describe NovelCompression::Compressor do
-	# \b?(\w+|[.,?!;:])\b?
-	# \b?(\w+|\p{Punct})\b?  -- probably gets punctuation I don’t want
-	it '#tokenize'
+	# /\b?(\w+|[.,?!;:])\b?/
+	# /\b?(\w+|\p{Punct})\b?/  -- probably gets punctuation I don’t want
+	it '#tokenize' do
+		input = StringIO.new "The quick brown fox jumps over the lazy dog.\nOr, did it?"
+		compressor = NovelCompression::Compressor.new input
+		tokens = compressor.tokenize
+		expect(tokens).to be_an(Array)
+		expect(tokens).to eq(['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog', '.', "\n", 'Or', ',', 'did', 'it', '?'])
+	end
 	it '#classify'
+	describe '#compress'
 end
