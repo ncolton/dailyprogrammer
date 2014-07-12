f49e67d5730f66a9e37e16c6437445ac376389c3
Nikolai Colton <ncolton@oddmagic.net>
2014-07-11 21:32:33 -0700

    handle hyphenation
    I'm learning more about RSpec and test structuring than anything else, at this point. Probably spending far more time than I should on it...

--- novel_compression/lib/novel_compression/compressor.rb
+++ novel_compression/lib/novel_compression/compressor.rb
@@ -11,7 +11,7 @@ module NovelCompression
 		end
 
 		def tokenize(s)
-			regex = /\b?(\w+|[.,?!;:\n])\b?/
+			regex = /\b?(\w+|[-.,?!;:\n])\b?/
 			tokens = []
 			tokens << s.scan(regex)
 			tokens.flatten!
--- novel_compression/spec/lib/novel_compression/compressor_spec.rb
+++ novel_compression/spec/lib/novel_compression/compressor_spec.rb
@@ -8,11 +8,25 @@ end
 describe NovelCompression::Compressor do
 	# /\b?(\w+|[.,?!;:])\b?/
 	# /\b?(\w+|\p{Punct})\b?/  -- probably gets punctuation I don’t want
-	it '#tokenize' do
-		input = "The quick brown fox jumps over the lazy dog.\nOr, did it?\n"
-		tokens = subject.tokenize input
-		expect(tokens).to be_an(Array)
-		expect(tokens).to eq(['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog', '.', "\n", 'Or', ',', 'did', 'it', '?', "\n"])
+	describe '#tokenize' do
+		it 'returns an array' do
+			expect(subject.tokenize '').to be_an(Array)
+		end
+
+		context 'when given a simple sentence' do
+			let(:input) { "The quick brown fox jumps over the lazy dog.\nOr, did it?\n" }
+			let(:result) { subject.tokenize input }
+
+			it 'returns the proper tokens' do
+				expect(result).to eq(['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog', '.', "\n", 'Or', ',', 'did', 'it', '?', "\n"])
+			end
+		end
+
+		context '"Waffle-maker"' do
+			let(:input) { "Waffle-maker" }
+			let(:result) { subject.tokenize input }
+			it { expect(result).to eq(['Waffle', '-', 'maker']) }
+		end
 	end
 
 	describe '#classify' do
